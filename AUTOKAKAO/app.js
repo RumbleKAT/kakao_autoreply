73d59c422d43c38fd2f1ba50e55827d07ed7e541
송명진 <reki318@naver.com>
2017-02-03 21:16:29 +0900

    complete

--- AUTOKAKAO/app.js
+++ AUTOKAKAO/app.js
@@ -140,6 +140,122 @@ app.post('/message',function(req, res){
                   });
                 })//http request
               }
+              //영한사전
+              else if(word[0] == "한영")
+              {
+                  var url = "http://endic.naver.com/translateAPI.nhn?sLn=kr&_callback=window.__jindo2_callback+"+"."+"$2431&m=getTranslate&query="+encodeURIComponent(word[1])+"&sl=ko&tl=en";
+                    console.log(url);
+                    // var result = {};
+                    var output = '';
+
+                    http.get(url,function(web){
+                      web.on('data',function(buffer){
+                        //  res.write(buffer);
+                        output += buffer;
+                      });
+                      web.on('end',function(){
+                        var object = output.toString();
+                        var a = object.search("resultData");
+                        var b = object.indexOf("dir",a);
+                        output = object.slice(a+13,b-3);
+                        //res.end();
+                      });
+                      fs.readFile( __dirname + "/data/message.json", 'utf8',  function(err, data){
+                        var messages = JSON.parse(data);
+                        // 이미지검색 결과 저장 및 번환
+                        messages["message"] = {"text" : output };
+                        fs.writeFile(__dirname + "/data/message.json",
+                               JSON.stringify(messages, null, '\t'), "utf8", function(err, data){
+                                 fs.readFile( __dirname + "/data/message.json", 'utf8', function (err, data) {
+                                  res.end(data);
+                                  return;
+                                })
+                        })
+                      })
+              })
+            }
+              else if(word[0] == "영한")
+              {
+                var url = "http://endic.naver.com/translateAPI.nhn?sLn=kr&_callback=window.__jindo2_callback+"+"."+"$2431&m=getTranslate&query="+encodeURIComponent(word[1])+"&sl=en&tl=ko";
+                  console.log(url);
+                  // var result = {};
+                  var output = '';
+
+                  http.get(url,function(web){
+                    web.on('data',function(buffer){
+                      //  res.write(buffer);
+                      output += buffer;
+                    });
+                    web.on('end',function(){
+                      var object = output.toString();
+                      var a = object.search("resultData");
+                      var b = object.indexOf("dir",a);
+                      output = object.slice(a+13,b-3);
+                      //res.end();
+                    });
+                    fs.readFile( __dirname + "/data/message.json", 'utf8',  function(err, data){
+                      var messages = JSON.parse(data);
+                      // 이미지검색 결과 저장 및 번환
+                      messages["message"] = {"text" : output };
+                      fs.writeFile(__dirname + "/data/message.json",
+                             JSON.stringify(messages, null, '\t'), "utf8", function(err, data){
+                               fs.readFile( __dirname + "/data/message.json", 'utf8', function (err, data) {
+                                res.end(data);
+                                return;
+                              })
+                      })
+                    })
+              })
+            }
+              else if (word[0] == "사전")
+              {
+                var client_id = 'ImKP3ZrLsLwL6hBgVedd';
+                var client_secret = 'ptElXa9Tz5';
+                var message = {};
+
+                var title ="";
+                var description="";
+                var link = "";
+
+                var api_url = "https://openapi.naver.com/v1/search/encyc?query=" + encodeURIComponent(word[1]);
+                var request = require('request');
+                var options = {
+                  url: api_url,
+                  headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+                };
+
+                request.get(options,function(error, response, body){
+                  if (!error && response.statusCode == 200) {
+                    res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+                    var a  = JSON.parse(body);
+                     title = a.items[0].title;
+                     description = a.items[0].description;
+                     link = a.items[0].link;
+
+                     console.log(title + description + link);
+                     var result = "제목: " + title + '\n' +"내용: " + description + "\n" + "링크: " + link;
+
+                     // 파일 입출력
+                     fs.readFile( __dirname + "/data/message.json", 'utf8',  function(err, data){
+                       var messages = JSON.parse(data);
+                       // 이미지검색 결과 저장 및 번환
+                       messages["message"] = {"text" : result};
+                       fs.writeFile(__dirname + "/data/message.json",
+                              JSON.stringify(messages, null, '\t'), "utf8", function(err, data){
+                                fs.readFile( __dirname + "/data/message.json", 'utf8', function (err, data) {
+                                 res.end(data);
+                                 return;
+                               })
+                       })
+                     })
+                    }
+                    else {
+                      res.status(response.statusCode).end();
+                      console.log('error = ' + response.statusCode);
+                    }
+
+                });
+              }
 
               else if(word[0] == "문장")
               {
@@ -160,10 +276,42 @@ app.post('/message',function(req, res){
                   web.on('end',function(){
                      object = output.toString();
                      var errorWordCount = object.search("errorWordCount");
+                     var numstart = object.indexOf(":",errorWordCount);
+                     var numend = object.indexOf(",",numstart);
+                     var num = object.slice(numstart+1, numend);
 
+                     var result = "";
 
-                    message["message"] = {"text" : object};
-                    res.json(message);
+                     if(num == 0)
+                     {
+                       message["message"] = {"text" : "틀린 문장이 없습니다."};
+                       res.json(message);
+                     }
+                       while(num != 0)
+                       {
+                         var helpMessage = object.search("helpMessage");
+                         var helpstart = object.indexOf(":",helpMessage);
+                         var helpend = object.indexOf("}",helpstart);
+                         var helpresult = object.slice(helpstart,helpend);
+
+                         object = object.replace("helpMessage","");
+                         object = object.replace(helpresult,"");
+
+                         result += "지적사항: " + helpresult ;
+                         num--;
+                       }
+                       var a = result.search("u00");
+
+                       while(a != -1)
+                       {
+                         result = result.replace("u00","");
+                         result = result.replace("\\27","");
+                         a = result.search("u00");
+                         console.log(a);
+                       }
+
+                       message["message"] = {"text" : result };
+                       res.json(message);
                   });
                 })//http request
               }
